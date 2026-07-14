e3244e71908a7d1f526f9fe7bd3552d8d70ebf4c
mehedy22 <mehedy.mmc272@gmail.com>
2026-07-14 08:40:16 +0600

    Implementing Head recursion

--- src/Algorithms/recursion/HeadRecursion.java
+++ src/Algorithms/recursion/HeadRecursion.java
@@ -1,4 +1,22 @@
 package Algorithms.recursion;
 
+/*
+The recursive function will execute first then it is called Head Recursion.
+No other statement will execute before the recursion call
+*/
 public class HeadRecursion {
+
+    static void function(int n) {
+        if (n > 0) {
+            function(n - 1);
+            System.out.println(n);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        function(n);
+
+    }
 }
