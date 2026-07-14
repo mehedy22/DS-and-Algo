721fcd5ee34336f888c14ca180c6c7293cc559e9
mehedy22 <mehedy.mmc272@gmail.com>
2026-07-14 08:43:40 +0600

    Implementing Tree recursion

--- src/Algorithms/recursion/TreeRecursion.java
+++ src/Algorithms/recursion/TreeRecursion.java
@@ -1,4 +1,21 @@
 package Algorithms.recursion;
 
+
+//if a recursive function calling itself more than once then it is called Tree Recursion
+
 public class TreeRecursion {
+
+    static void function(int n){
+        if(n>0){
+            System.out.println(n);
+            function(n-1);
+            function(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n=3;
+        function(n);
+    }
+
 }
