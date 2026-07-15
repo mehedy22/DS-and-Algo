33a9ea165eb22be2401dcbc629245c45ffb786e5
mehedy22 <mehedy.cuet.cse@gmail.com>
2026-07-15 22:45:47 +0600

    Add Indirect Recursion Demo

---
+++ src/Algorithms/recursion/IndirectRecursion.java
@@ -0,0 +1,24 @@
+package Algorithms.recursion;
+
+public class IndirectRecursion {
+
+
+    static void funA(int n) {
+        if (n > 0) {
+            System.out.print(n + " ");
+            funB(n - 1);
+        }
+    }
+
+    static void funB(int n) {
+        if (n > 1) {
+            System.out.print(n + " ");
+            funA(n / 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        funA(20);
+    }
+
+}
