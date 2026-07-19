fab9b4e46d429633323bb519ca350aa4a42d41c1
mehedy22 <mehedy.mmc272@gmail.com>
2026-07-19 12:49:26 +0600

    Implementing fibonacci using recursion

--- src/Algorithms/recursion/problems/FibonacciNumber.java
+++ src/Algorithms/recursion/problems/FibonacciNumber.java
@@ -2,15 +2,15 @@ package Algorithms.recursion.problems;
 
 public class FibonacciNumber {
 
+    static int fibonacci(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(fibonacci(10));
-    }
-    static int fibonacci(int n){
-        if(n<2){
-            return n;
-        }
-        else{
-            return fibonacci(n-1)+fibonacci(n-2);
-        }
     }
 }
