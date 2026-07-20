1cd3a30f880945ee562ee75b3a99fddbf8e6d77b
mehedy22 <mehedy.cuet.cse@gmail.com>
2026-07-20 21:47:17 +0600

    Implement wheather a number is power of three or not

---
+++ src/Algorithms/recursion/problems/IsPowerOfThree.java
@@ -0,0 +1,12 @@
+package Algorithms.recursion.problems;
+
+public class IsPowerOfThree {
+    public boolean isPowerOfThree(int n) {
+        if (n == 1) {
+            return true;
+        } else if (n <= 0 || n % 3 != 0) {
+            return false;
+        }
+        return isPowerOfThree(n / 3);
+    }
+}
