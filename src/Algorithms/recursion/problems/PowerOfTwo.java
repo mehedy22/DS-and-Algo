d7429b1d22addbc892abc5fff3b16792a3ee3619
mehedy22 <mehedy.cuet.cse@gmail.com>
2026-07-20 21:38:52 +0600

    Implement wheather a number is power of two or not

--- src/Algorithms/recursion/problems/PowerOfTwo.java
+++ src/Algorithms/recursion/problems/PowerOfTwo.java
@@ -2,13 +2,12 @@ package Algorithms.recursion.problems;
 
 public class PowerOfTwo {
     static boolean isPowerOfTwo(int n) {
-        if(n==1){
+        if (n == 1) {
             return true;
-        }
-        else if(n<=0 ||n%2!=0){
+        } else if (n <= 0 || n % 2 != 0) {
             return false;
         }
-        return isPowerOfTwo(n/2);
+        return isPowerOfTwo(n / 2);
     }
 
     public static void main(String[] args) {
