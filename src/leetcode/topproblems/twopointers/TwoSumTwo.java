11b356278b6e6dcc1378b6cb89060c7232a51ce6
mehedy22 <mehedy.mmc272@gmail.com>
2026-07-22 10:06:33 +0600

    Implement Two Sum II using two-pointer technique

---
+++ src/leetcode/topproblems/twopointers/TwoSumTwo.java
@@ -0,0 +1,22 @@
+package leetcode.topproblems.twopointers;
+
+public class TwoSumTwo {
+
+
+    public int[] twoSum(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+
+        while (i < j) {
+            if (nums[i] + nums[j] > target) {
+                j--;
+            } else if (nums[i] + nums[j] < target) {
+                i++;
+            } else {
+                return new int[]{i + 1, j + 1};
+            }
+        }
+        return new int[]{};
+    }
+
+}
