eae2db43c1a4b7b9b666b8426097324be9e802b4
mehedy22 <mehedy.cuet.cse@gmail.com>
2026-07-23 20:43:09 +0600

    Implement min stack

---
+++ src/leetcode/topproblems/stack/MinStack.java
@@ -0,0 +1,46 @@
+package leetcode.topproblems.stack;
+
+import java.util.Stack;
+
+public class MinStack {
+
+    Stack<Integer> mainStack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        mainStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        mainStack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int popValue = mainStack.pop();
+        if (popValue == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return mainStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
